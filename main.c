4b219b630757ce7ba9338a915558ee4b608298e7
Francois Van Laer <fvl209@nyu.edu>
2019-09-26 12:41:38 -0400

    First file opened

--- main.c
+++ main.c
@@ -7,6 +7,13 @@
 
 int main(){
 
-    printf("Hello World");
+    printf("WELCOME TO OUR SPELL CHECK\n");
+
+    hashmap_t hashtable[HASH_SIZE];
+
+    bool dict_read = load_dictionary("C:\\Users\\Francois\\CLionProjects\\Application Security\\AppSec-Assignment1\\wordlist.txt", hashtable);
+
+    fputs(dict_read ? "true" : "false", stdout);
+
     return 0;
 }
--- spell.c
+++ spell.c
@@ -1,3 +1,8 @@
+// Francois VAN LAER
+// N14816307
+// New York University
+// CS 9163 - Application Security
+
 #include<stdio.h>
 #include<stdbool.h>
 #include <stdlib.h>
@@ -8,8 +13,7 @@
 #define HASH_SIZE 2000
 #define MAX_MISSPELLED 1000
 
-typedef struct node
-{
+typedef struct node{
     char word[LENGTH + 1];
     struct node* next;
 }
@@ -17,8 +21,7 @@ node;
 
 typedef node* hashmap_t;
 
-int hash_function(const char* word)
-{
+int hash_function(const char* word){
     int sum = 0;
     int word_length = strlen(word);
 
@@ -29,9 +32,8 @@ int hash_function(const char* word)
     return bucket;
 }
 
-bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
-{
-    // Initialize hashtable
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){
+    // Initialize / Reset hashtable
     for (int i = 0; i < HASH_SIZE; i++){
         hashtable[i] = NULL;
     }
@@ -49,7 +51,7 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
     // Read each word of dictionary    
     while (fscanf(fp, "%s", temp)==1){
         // Create new node
-        node* new_node = malloc(sizeof(node);
+        node* new_node = malloc(sizeof(node));
         new_node->next = NULL;
         // Copy word from 'temp' to 'new_node'
         strcpy(new_node->word, temp);
@@ -70,37 +72,35 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
     return true;
 }
 
-bool check_word(const char* word, hashmap_t hashtable[])
-{
-    int bucket = hash_function(word);
-    hashmap_t* cursor = hashtable[bucket];
-
-    while(cursor != NULL){
-        if(strcmp(word, cursor->word) == 0){
-            return true;
-        }
-        cursor = cursor->next;
-    }
-    return false;
-}
-
-int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
-{
-    int num_misspelled = 0;
-
-    char temp[LENGTH + 1];
-    char line[LENGTH + 1];
-
-    while (fgets(temp, LENGTH + 1, fp) != NULL){
-        strcpy(line, temp);
-        char* word = strtok(line, " ,-;:");
-        while (word != NULL){
-            if (check_word(word) == false){
-                strcat(misspelled, word);
-                strcat(misspelled, " ");
-                num_misspelled++;
-            }
-        }
-    }
-    return num_misspelled;
-}
+//bool check_word(const char* word, hashmap_t hashtable[]){
+//    int bucket = hash_function(word);
+//    hashmap_t* cursor = hashtable[bucket];
+//
+//    while(cursor != NULL){
+//        if(strcmp(word, cursor->word) == 0){
+//            return true;
+//        }
+//        cursor = cursor->next;
+//    }
+//    return false;
+//}
+//
+//int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]){
+//    int num_misspelled = 0;
+//
+//    char temp[LENGTH + 1];
+//    char line[LENGTH + 1];
+//
+//    while (fgets(temp, LENGTH + 1, fp) != NULL){
+//        strcpy(line, temp);
+//        char* word = strtok(line, " ,-;:");
+//        while (word != NULL){
+//            if (check_word(word) == false){
+//                strcat(misspelled, word);
+//                strcat(misspelled, " ");
+//                num_misspelled++;
+//            }
+//        }
+//    }
+//    return num_misspelled;
+//}
